ec01da5d79f25d09d5ffd0515209a2b0b238f066
Seungwoo Lim <daniel73919@gmail.com>
2021-11-03 22:59:33 +0900

    Create makerwidget.js

---
+++ assets/js/plugins/makerwidget.js
@@ -0,0 +1,12 @@
+(function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "left",          
+            widget: "rrdv8s2wjy0yylb8-xt11zgislbom1j3o-m1neb1metxvkupww"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"))
